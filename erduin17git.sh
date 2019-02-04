72b958107928eead900ec4788e4d468787d0297d
erduin17 <erduinmacias.cs@gmail.com>
2019-02-04 08:47:08 -0800

    AA

---
+++ erduin17git.sh
@@ -0,0 +1,6 @@
+git config --list;
+git config --global user.username  erduin17;
+git config --global user.name  erduin17;
+git config --global user.user erduin17;
+git config --global user.email erduinmacias.cs@gmail.com;
+git config --list;
